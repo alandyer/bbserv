89559b84a6586e3ec277018d6f4d0cfc3a0ec545
Alan Dyer <dyer.alan@gmail.com>
2013-08-03 10:49:39 -0300

    WIP achat controller

---
+++ src/controller/bbserv_achat_controller.erl
@@ -0,0 +1,26 @@
+-module(bbserv_achat_controller, [Req]).
+-compile(export_all).
+%-behaviour(gen_server).
+
+
+
+%login('GET', []) -> ok;
+
+%login('POST', []) ->
+%  LoginName = Req:post_param("login_name"),
+%  cast(bbserv_achat_controller, login). 
+  
+
+start_link(bbserv_achat_controller, _Args, _Options) -> ok.
+
+init(Args) ->
+  ok.
+
+%handle_call(Request, State) ->
+%  case Request of
+%    {login} ->
+      
+  
+  
+
+
