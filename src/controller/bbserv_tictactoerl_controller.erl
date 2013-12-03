07f828e360a81263f76d58353d59396df56cfb04
Alan Dyer <dyer.alan@gmail.com>
2013-12-03 12:02:14 -0400

    WIP: gating multiple login

--- src/controller/bbserv_tictactoerl_controller.erl
+++ src/controller/bbserv_tictactoerl_controller.erl
@@ -1,16 +1,24 @@
--module(bbserv_tictactoe_controller, [Reqs, SessionID]).
+-module(bbserv_tictactoerl_controller, [Reqs, SessionID]).
 -compile(export_all).
 
 
 
 login('GET', []) -> ok;
 login('POST', []) ->
-  Logname = Reqs:post_param("login_name"),
-  boss_session:set_session_data(SessionID, Logname, self()),
-  {redirect, [{action, "list"}]}.
+  LoggedInAlready = boss_session:get_session_data(SessionID, login),
+  case LoggedInAlready of
+    {OldLogName} ->
+      {ok, [{loggedinalready, true}]};
+    {error, Reason} ->
+      Logname = Reqs:post_param("login_name"),
+      boss_session:set_session_data(SessionID, login, Logname),
+      {redirect, [{action, "list"}]}
+  end.
 
 list('GET', []) ->
-  Sessions = boss_session:get_session_data(SessionID),
-  {ok, [{sessions, Sessions}]}.
+  Sessionvars = boss_session:get_session_data(SessionID),
+  {ok, [{sessionvars, Sessionvars}]}.
+
+
 
 
